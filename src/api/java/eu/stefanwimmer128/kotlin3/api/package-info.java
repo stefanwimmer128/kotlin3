ba6d84f6d224c385d619d35f7e1d9f48502b62f4
Stefan Wimmer <info@stefanwimmer128.eu>
2018-12-30 03:00:01 +0100

    v1.1.0

--- src/api/java/eu/stefanwimmer128/kotlin3/api/package-info.java
+++
@@ -1,4 +0,0 @@
-@API(owner = Kotlin3API.MODID, provides = Kotlin3API.MODID + ".api", apiVersion = Kotlin3API.VERSION)
-package eu.stefanwimmer128.kotlin3.api;
-
-import net.minecraftforge.fml.common.API;
